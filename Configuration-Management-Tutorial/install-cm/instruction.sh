30f86680bb0499a3f3f11736bc7cd734139cfb1b
Matt <Matt>
2023-10-25 08:07:18 +0000

    Add and modify files

--- Configuration-Management-Tutorial/install-cm/instruction.sh
+++ Configuration-Management-Tutorial/install-cm/instruction.sh
@@ -1,11 +1,12 @@
 # add hosts here (create if there's no directory): /etc/ansible/hosts
+# sudo nano /etc/ansible/hosts
 ansible all --list-hosts
 
 # Set up SSH Connections
 # To generate SSH Key
 ssh-keygen
-cd .ssh/
-cat id_rsa.pub
+# cd ~/.ssh/
+cat ~/.ssh/id_rsa.pub
 
 # Copy the key to another machine
 nano .ssh/authorized_keys
