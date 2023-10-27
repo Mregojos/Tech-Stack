332b1263bd21d8eeca89456817992f0bfb8ee571
Matt <Matt>
2023-10-27 01:52:03 +0000

    Add and modify files

---
+++ Container-Orchestration-Tutorial/install-kubernetes/install-co.sh
@@ -0,0 +1,7 @@
+# Reference: kubernetes.io/docs
+
+## Install Minkinube
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+# Start Minikube
+minikube start
