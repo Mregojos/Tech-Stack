d4824aa967343425b8eed0a2630889210cc09fad
Matt <Matt>
2023-10-28 06:24:20 +0000

    Add and modify files

--- sh-scripts/sh-scripts.sh
+++ sh-scripts/sh-scripts.sh
@@ -1,7 +1,17 @@
 touch <File>
 mkdir <Dir>
+pwd 
 
 rm [-r|f] <Dir>
 rm <File>
 
 echo $var
+echo "<string>" > <file> # Add
+echo "<string>" >> <file> # Insert
+
+cp -rf <dir>/* <dir>
+
+mv <file_name> <new_file_name>
+
+
+
