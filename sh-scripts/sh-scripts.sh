3d5378c015c1dfec29cafa9e53561ac84960f2f2
Matt <Matt>
2023-10-28 06:28:19 +0000

    Add and modify files

--- sh-scripts/sh-scripts.sh
+++ sh-scripts/sh-scripts.sh
@@ -1,17 +1,34 @@
+# Create a new file
 touch <File>
+
+# Create a new directory
 mkdir <Dir>
+
+# Print working directory
 pwd 
 
-rm [-r|f] <Dir>
+# Remove a file
 rm <File>
+# Remove a directory
+rm [-r|f] <Dir>
 
+# Print
 echo $var
+# Add or Insert
 echo "<string>" > <file> # Add
 echo "<string>" >> <file> # Insert
 
+# Copy
+cp <file> <dir>
+# Copy recursively
 cp -rf <dir>/* <dir>
 
+# Rename or move a file or a directory
 mv <file_name> <new_file_name>
+
+# Placeholder
+# All e.g., cd 1*e (1-Sample)
+* 
 
 
 
