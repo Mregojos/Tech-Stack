52c076fe00c9341fe1366f3f7b32cfbd08dc8aae
Matt <Matt>
2023-10-29 03:33:20 +0000

    Add and modify files

---
+++ sh-scripts/gcloud-scripts.sh
@@ -0,0 +1,26 @@
+# Install gcloud
+
+# Initialize gcloud
+gcloud init
+
+# gcloud version
+gcloud version
+
+# gcloud help
+gcloud help
+
+# gloud authentication
+
+# gcloud projects
+# PROJECT_ID should be unique
+gcloud projects create <PROJECT_ID> --name=<PROJECT_NAME>
+
+# gcloud config
+gcloud config get list
+
+--------------------------------------------
+# Other Google Cloud CLI (gsutil, bq)
+
+# gsutil
+
+# bq
