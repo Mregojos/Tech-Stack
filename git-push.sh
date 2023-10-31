a568021ea22da4b40f94adb5e1e6153622e56d7c
Mregojos <jupyter@matt-nb.us-central1-a.c.training1029.internal>
2023-10-31 08:08:11 +0000

    Edit a file

--- git-push.sh
+++ git-push.sh
@@ -2,7 +2,7 @@
 # sh git-push.sh
 
 git add .
-git config --global user.email "Matt"
+# git config --global user.email "Matt"
 git config --global user.name "Mregojos"
 git commit -m "Add and modify files"
 git push
