a41be70803bc9b7506b41715ad463355f34bc1b9
Mregojos <Matt>
2023-10-31 08:24:34 +0000

    Email

--- git-push.sh
+++ git-push.sh
@@ -2,7 +2,7 @@
 # sh git-push.sh
 
 git add .
-git config user.email "Mregojos"
+git config user.email "matt@example.com"
 git config user.name "Mregojos"
 git commit -m "Add and modify files"
 git push
