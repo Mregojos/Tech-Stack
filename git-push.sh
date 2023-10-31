bbfe86af67b70a06ef5a2608211d94c637729eda
MRegojos <Matt>
2023-10-31 08:28:34 +0000

    Add and modify files

--- git-push.sh
+++ git-push.sh
@@ -2,8 +2,8 @@
 # sh git-push.sh
 
 git add .
-git config --global user.email "Matt"
-git config --global user.name "MRegojos"
+# git config --global user.email "Matt"
+# git config --global user.name "MRegojos"
 git commit -m "Add and modify files"
 git push
 
