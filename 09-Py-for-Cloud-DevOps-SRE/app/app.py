7ddcf6a57b19447c24e5afcc5d135d532cf9d59d
Matt <mattiregojos@gmail.com>
2023-11-05 03:21:52 +0000

    Add and modify files

--- 09-Py-for-Cloud-DevOps-SRE/app/app.py
+++ 09-Py-for-Cloud-DevOps-SRE/app/app.py
@@ -1,3 +1,15 @@
 # Simple Streamlit App Docker Python
 import streamlit as st
-st.text("It's working.")
+st.subheader("Simple Notepad :notebook:")
+st.caption("Add your thoughts here! It will be stored in a database!")
+st.subheader("",divider="rainbow")
+
+name = st.text_input("Your Name here")
+note = st.text_area("Add Note here")
+if st.button("Add a note"):
+    st.write(name,":", note)
+    st.balloons()
+    
+st.subheader("",divider="rainbow")
+st.write("**Previous Notes**")
+
